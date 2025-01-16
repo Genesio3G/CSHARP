54a7123cc11e64f1e36150cc2c53a6b9f4f8df3a
genesiogabriel <genesiogabriel101998@hotmail.com>
2025-01-16 20:26:24 +0100

    adicionando arquivo de array

---
+++ Array/Program.cs
@@ -0,0 +1,5 @@
+﻿string[] nomeAluno = {"Genesio","Belita","Jessica"};
+
+for(int contador = 0; contador < nomeAluno.Length; contador++){
+    Console.WriteLine(" Posição do Aluno "+contador+" Nome do Aluno "+ nomeAluno[contador]);
+}
